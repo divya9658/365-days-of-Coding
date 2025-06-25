cbccc8767eb40aacb5cc67567910e54b737aa232
divya9658 <23mh1a05m4@acoe.edu.in>
2025-06-25 13:34:08 +0530

    Update IsPrime.cpp

--- IsPrime.cpp
+++ IsPrime.cpp
@@ -1,6 +1,9 @@
+//header file that includes input output functions, math functions etc.
 #include<bits/stdc++.h>
-using namespace std;
-bool isPrime(int n){
+using namespace std; // Instead of writing std:cout<< we can just use cout<<
+
+ // bool function returns whether it is prime or not
+bool isPrime(int n){  
     if(n<2) return false;
     for(int i=2;i<=sqrt(n);i++){
         if(n%i==0) return false;
