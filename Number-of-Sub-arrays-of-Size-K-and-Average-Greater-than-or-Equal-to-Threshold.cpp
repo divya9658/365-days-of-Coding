91c6090bfb69589a04de0811e74f12139318359d
divya9658 <23mh1a05m4@acoe.edu.in>
2025-09-20 21:39:30 +0530

    [1343] [Time Beats: 100.00%] [Memory Beats: 64.92%] - LeetPush

---
+++ Number-of-Sub-arrays-of-Size-K-and-Average-Greater-than-or-Equal-to-Threshold.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numOfSubarrays(vector<int>& arr, int k, int threshold) {
+        int count=0;
+        int sum=0;
+        for(int i=0;i<k;i++){
+            sum+=arr[i];
+        }
+        if(sum >= threshold*k) count++;
+        for(int i=k;i<arr.size();i++){
+            sum += arr[i] - arr[i-k];
+            if(sum >= threshold*k) count++;
+        }
+        return count;
+    }
+};
